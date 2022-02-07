5d930b6d361e53c6a6d9ca5104a2373bb7c775e3
stevleibelt <stev_leibelt@freiberg.de>
2022-02-07 13:37:32 +0100

    added option to run the iso

--- build.sh
+++ build.sh
@@ -152,6 +152,45 @@ function exit_if_not_called_from_root ()
     #end of check if we are root
 }
 
+function run_iso_if_wanted ()
+{
+    local PATH_TO_THE_ISO=${1:-""}
+
+    if [[ -f "${PATH_TO_THE_ISO}" ]];
+    then
+        echo ":: Do you want to run the iso for testing? [y|N]"
+        read RUN_ISO
+
+        if [[ ${RUN_ISO} == "y" ]];
+        then
+            if [[ ! -d "/usr/share/qemu" ]];
+            then
+                echo ":: qemu package is missing, installing it ..."
+                pacman -S qemu
+            fi
+
+            if [[ ! -d "/usr/share/edk2-ovmf" ]];
+            then
+                echo ":: edk2-ovmf package is missing, installing it ..."
+                pacman -S edk2-ovmf
+            fi
+
+            echo ":: Do you want to run it as UEFI? [y|N]"
+            read RUN_AS_UEFI
+
+            if [[ ${RUN_AS_UEFI} == "y" ]];
+            then
+                run_archiso -u -i ${PATH_TO_THE_ISO}
+            else
+                run_archiso -i ${PATH_TO_THE_ISO}
+            fi
+        fi
+    else
+        echo ":: Invalid path provided."
+        echo "   >>${PATH_TO_THE_ISO}<< is not a valid file."
+    fi
+}
+
 function setup_environment ()
 {
     #begin of check if archiso is installed
@@ -224,6 +263,7 @@ function _main ()
     add_packages_and_repository
     cleanup_build_path
     build_archiso
+    run_iso_if_wanted
 
     cd ${CURRENT_WORKING_DIRECTORY}
 }
